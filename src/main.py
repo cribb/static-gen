42905eef9fb1f2d85bd38c498d705b785e3193dc
Jeremy Cribb <cribb.jeremy@gmail.com>
2025-11-20 16:42:13 -0500

    Adding initial static site rendering routines by moving data into public directory.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
+import os
+import shutil
 from textnode import TextNode, TextType
+from htmlnode import ParentNode, LeafNode
+from md_handler import *
+
+def prep_public_path(static_path, public_path):
+
+    if os.path.exists(public_path):
+        print("Removing public path for freshening.")
+        shutil.rmtree(public_path)
+
+    # shutil.copytree(static_path, public_path)
+    
+
 def main():
-    anchortext = "This is some anchor text"
-    url = "https://www.boot.dev"
-    tn = TextNode(text=anchortext, 
-                  text_type=TextType.LINK, 
-                  url=url)
-
-    print(tn)
-
+    root_path = "/home/jeremy/src/bootdev/static-gen/"
+    static_path = rootpath + "static/"
+    public_path = rootpath + "public/"
+    
+    prep_public_path(static_path, public_path)
 
 
 
